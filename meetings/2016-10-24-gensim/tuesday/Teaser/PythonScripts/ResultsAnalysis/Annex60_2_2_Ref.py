ab09cf6ef9584843ddec02ffcef4f78534a443fe
mlauster <mlauster@eonerc.rwth-aachen.de>
2016-10-20 14:18:32 +0200

    Adds results figure and updated version of result analysis script

--- meetings/2016-10-24-gensim/tuesday/Teaser/PythonScripts/ResultsAnalysis/Annex60_2_2_Ref.py
+++ meetings/2016-10-24-gensim/tuesday/Teaser/PythonScripts/ResultsAnalysis/Annex60_2_2_Ref.py
@@ -4,53 +4,53 @@ This module contains the building results class to process simulation data
 
 """
 
-import pandas as pd
-from modelicares import SimRes
+import modelicares as mr
 import os
-import glob
-import numpy as np
+import matplotlib.pyplot as plt
 
-'''Set pathes'''
+'''Set paths'''
 
 relative_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 results_path = os.path.join(relative_path, 'Results', 'Annex60_2_2_Ref')
 
-'''Load results'''
+'''Load results for both buildings we would like to compare'''
 
-# simulations = []
-#
-# '''Find all simulation results files in results_path'''
-#
-# search_str = results_path + '/*.mat'
-# simulation_paths = glob.glob(search_str)
-#
-# for simulation_path in simulation_paths:
-#     simulations.append(SimRes(simulation_path))
-#
-# '''Find both buildings we would like to compare'''
-#
-# D1_North_1 = []
-# D2_North_1 = []
-#
-# for simulation in simulations:
-#     if 'D1_North1' in simulation.fbase:
-#         D1_North_1.append(simulation)
-#     elif 'D2_North1' in simulation.fbase:
-#         D2_North_1.append(simulation)
-#
-# '''Identify indoor air temperature'''
-#
-# T_Air_string = '*.TAir'
-#
-# T_Air_variables_D1_North1 = []
-# for simulation in D1_North_1:
-#     T_Air_variables_D1_North1.append(simulation.names(T_Air_string))
-#
-# T_Air_variables_D2_North1 = []
-# for simulation in D2_North_1:
-#     T_Air_variables_D2_North1.append(simulation.names(T_Air_string))
-#
-#
-# print(T_Air_variables_D1_North_1)
-# print(T_Air_variables_D2_North_1)
-# print(len(simulations))
+simulations = mr.SimResList(results_path + '/D*_North1_*')
+
+'''Extract variables and mean values'''
+
+print('Mean temperatures of well insulated building, living room: ',
+      simulations[simulations.fnames.index(
+      'D1_North1_LivingRoom.mat')][
+      'thermalZoneTwoElements.TAir'].mean(), ', bed room: ',
+      simulations[simulations.fnames.index(
+      'D1_North1_BedRoom.mat')][
+      'thermalZoneTwoElements.TAir'].mean(), ', sanitary room: ',
+      simulations[simulations.fnames.index(
+      'D1_North1_WCandsanitaryroomsinnonresidentialbuildings.mat')][
+      'thermalZoneTwoElements.TAir'].mean())
+
+print('Mean temperatures of bad insulated building,  living room: ',
+      simulations[simulations.fnames.index(
+      'D2_North1_LivingRoom.mat')][
+      'thermalZoneTwoElements.TAir'].mean(), ', bed room: ',
+      simulations[simulations.fnames.index(
+      'D2_North1_BedRoom.mat')][
+      'thermalZoneTwoElements.TAir'].mean(), ', sanitary room: ',
+      simulations[simulations.fnames.index(
+      'D2_North1_WCandsanitaryroomsinnonresidentialbuildings.mat')][
+      'thermalZoneTwoElements.TAir'].mean())
+
+'''Plot living room temperature profiles'''
+
+extracted_simulations = mr.SimResList([
+      simulations[simulations.fnames.index(
+      'D1_North1_LivingRoom.mat')],
+      simulations[simulations.fnames.index(
+      'D2_North1_LivingRoom.mat')]])
+
+extracted_simulations.plot('thermalZoneTwoElements.TAir',
+                           ylabel1="Temperature",
+                           leg1_kwargs=dict(loc='upper right'),
+                           title="Temperatures of living rooms")
+plt.show()
