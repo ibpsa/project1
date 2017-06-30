d479e89b6d7e87f1dcba8297428438d6a9f4ca21
Michael Wetter <mwetter@lbl.gov>
2017-06-30 08:23:28 -0700

    Updated compilation script for new jekyll

--- compile.sh
+++ compile.sh
@@ -1,5 +1,1 @@
-if [ `uname` == "Darwin" ]; then
-    jekyll serve --config _config.yml,_config_dev.yml
-else
-   jekyll --future --server 4000 --url http://127.0.0.1:4000/modelica
-fi;
+jekyll serve --config _config.yml,_config_dev.yml
